c98e91f1e8d85f2594857c045f1d3faa5534d3ee
thatSteveFan <pnaduthota@google.com>
2022-08-03 22:24:24 +0000

    fixed binary name

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 
 #clang -lelf -lbpf make_map.c -o make_map.o -ggdb3 
 clang -Wall socket.c -o socket.o -fsanitize=address -ggdb3
-clang -Wall client.c -o client.o -static -flto -O3
+clang -Wall guest.c -o guest.o -static -flto -O3
 clang -O2 -g -Wall -target bpf -c host_redirect.c -o xdp_prog.o
 clang -O2 -g -Wall -target bpf -c guest_redirect.c -o guest_prog.o
 
-mv ./client.o /tmp/container1/client.o
+mv ./guest.o /tmp/container1/guest.o
